dd6cbbed945c68b36a35ffe16dbb7985c34c2d71
MikeHenderson4606 <futbolmike4606@gmail.com>
2024-07-18 06:49:36 -0400

    Implemented tab retrieval

--- ultimateguitar/urls.py
+++ ultimateguitar/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("queryTabs", views.queryTabs, name="queryTabs")
+    path("queryTabs", views.queryTabs, name="queryTabs"),
+    path("getTabInfo", views.getTabInfo, name="getTabInfo"),
+    path("getTotalTabs", views.getTotalTabNum, name="getTotalTabs")
 ]
--- ultimateguitar/views.py
+++ ultimateguitar/views.py
@@ -10,6 +10,8 @@ import json
 
 # Create your views here.
 
+totalTabNum = 0
+
 def index(request):
     return HttpResponse("You made it to the ultimate guitar backend!")
 
@@ -37,7 +39,6 @@ def index(request):
     # return HttpResponse(200)
 
 def queryTabs(request):
-    print(request.GET)
     baseURL = 'https://www.ultimate-guitar.com/search.php?'
     value = request.GET.get('value')
     searchType = request.GET.get('search_type')
@@ -45,9 +46,14 @@ def queryTabs(request):
 
     formattedURL = baseURL + 'page=' + page + '&search_type=' + searchType + '&value=' + value
     response = requests.get(formattedURL)
+
     html = response.text
+
+    totalTabNum = _findTotalTabNumber(html)
+
     data = _getQueryResults(html)
-    removedProTabs = []
+
+    removedProTabs = [[totalTabNum]]
     for elem in data:
         try:
             elem['marketing_type']
@@ -56,8 +62,45 @@ def queryTabs(request):
                 removedProTabs.append(elem)
     return HttpResponse(json.dumps(removedProTabs))
 
+def _findTotalTabNumber(html):
+    soup = BeautifulSoup(html, features="lxml")
+
+    titleString = str(soup.find("title"))
+    
+    titleList = titleString.split(" ")
+    indexOfChords = titleList.index("&amp;")
+    totalTabs = titleList[indexOfChords - 2]
+
+    return int(totalTabs)
+
 def _getQueryResults(html):
+    soup = BeautifulSoup(html, features="lxml")
+
+    dataDiv = soup.find("div", {"class": "js-store"})
+    data = json.loads(dataDiv['data-content'])
+
+    return data['store']['page']['data']['results']
+
+def getTabInfo(request):
+    tabURL = request.GET.get('tabURL')
+
+    response = requests.get(tabURL)
+    html = response.text
+    raw_data = _getHTMLTabData(html)
+    print(json.dumps(raw_data))
+    return HttpResponse(json.dumps(raw_data))
+
+def _getHTMLTabData(html):
     soup = BeautifulSoup(html)
     dataDiv = soup.find("div", {"class": "js-store"})
     data = json.loads(dataDiv['data-content'])
-    return data['store']['page']['data']['results']
+    tabDict = {
+        'tabText': data['store']['page']['data']['tab_view']['wiki_tab']['content'],
+        'artistName': data['store']['page']['data']['tab']['artist_name'],
+        'songName': data['store']['page']['data']['tab']['song_name'],
+        'songUrl': data['store']['page']['data']['tab']['tab_url']
+    }
+    return tabDict
+
+def getTotalTabNum(request):
+    return HttpResponse(request.session['totalTabs'])
